c6b0298d4e8b5a04719358fbb47342eae4e14476
quentinLeDilavrec <ledilavrecquentin@gmail.com>
2017-09-17 19:20:00 +0200

    factor tools for divide

--- penrose.ml
+++ penrose.ml
@@ -7,50 +7,49 @@ let height = 1000;;
 
 open_graph (" "^(string_of_int width)^"x"^(string_of_int height)^"+0-0");;
 
+
 let phi = (1.+.(sqrt 5.))/. 2.;;
 
 let obtuse = true
 let acute  = false
 
-let random_color () =
-  let r = (Random.int 51)*5 in
-  begin
-    r
-  end;;
-
 let set_random_color() =
-  let r = random_color() in
-  let g = random_color() in
-  let b = random_color() in
+  let r = Random.int 255 in
+  let g = Random.int 255 in
+  let b = Random.int 255 in
   set_color (rgb r g b);; 
 
 let distance (ax,ay) (bx,by) = sqrt((ax -. bx)**2. +. (ay -. by)**2.);;
 
 let split_line (p1x,p1y) (p2x,p2y) =
-  let dist=(distance (p1x,p1y) (p2x,p2y))/.((1.+.(sqrt 5.))/. 2.) in
+  let dist=(distance (p1x,p1y) (p2x,p2y)) in
   let k2 = dist /. phi in
   let k1 = dist -. k2 in
-  ((p1x,p1y),((k1*.p2x+.k2*.p1x)/.(k1+.k2),(k1*.p2y+.k2*.p1y)/.(k1+.k2)),(p2x,p2y));;
+  let k1_plus_k2=k1+.k2 in (*factor or not?*)
+  ((p1x,p1y),
+   ((k1*.p2x+.k2*.p1x)/.k1_plus_k2,
+    (k1*.p2y+.k2*.p1y)/.k1_plus_k2),
+   (p2x,p2y));;
 
-let float_point_to_int (x,y)=
-  (int_of_float x, int_of_float y)
+let apply_to_point f (x,y) =
+  (f x, f y);;
 
-let float_tri_to_int p: (int*int) array =
-  [|float_point_to_int p.(0);float_point_to_int p.(1);float_point_to_int p.(2)|];;
+let apply_to_tri f p =
+  [|f p.(0);f p.(1);f p.(2)|];;
 
-
-Random.self_init;;
+let float_tri_to_int p : (int*int) array =
+  apply_to_tri (apply_to_point int_of_float) p;;
 
 (*pre-cond:
     p: apex is always the first point*)
-let rec divide gen div [|apex;s1;s2|] tyipe =
+let rec divide gen div [|apex;s1;s2|] triangle_type =
   (**)
   if gen <= 0 then 
     begin
       set_random_color();
       fill_poly (float_tri_to_int [|apex;s1;s2|]);
     end
-  else if tyipe == obtuse
+  else if triangle_type == obtuse
   then
     let (o_l, btw, a_h) = split_line s1 s2 in
     let a = [|a_h; apex; btw|] in
@@ -82,6 +81,9 @@ let rec divide gen div [|apex;s1;s2|] tyipe =
       divide (gen-1) div a2 acute;
       divide (gen-1) div o obtuse;
     end;;
+
+
+Random.self_init;;
 
 let get_x (x,y) = x;;
 let get_y (x,y) = y;;
