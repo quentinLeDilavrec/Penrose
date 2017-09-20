9865e7a0afb13c7287fa4aaf7dc7c90d404799aa
quentinLeDilavrec <ledilavrecquentin@gmail.com>
2017-09-21 01:42:02 +0200

    test on generation in divide is now make in a match

--- penrose.ml
+++ penrose.ml
@@ -17,7 +17,8 @@ let start_with_acute_triangle = true;;
 type point = float * float;;
 type triangle = point * point * point;;
 type triangle_type = Acute | Obtuse;;
-type penrose_triangle = {
+type penrose_triangle = triangle * triangle_type;;
+type penrose_triangle_old = {
   points:triangle;
   ttype:triangle_type
 }
@@ -37,14 +38,14 @@ let set_random_color() =
 let distance ((ax,ay):point) ((bx,by):point) = 
   sqrt((ax -. bx)**2. +. (ay -. by)**2.);;
 
-
-let split_line ((p1x,p1y):point) ((p2x,p2y):point) : point =
-  let dist = distance (p1x,p1y) (p2x,p2y) in
+(* return the point on ab at |ab|/phi from a *)
+let split_line ((ax,ay):point) ((bx,by):point) : point =
+  let dist = distance (ax,ay) (bx,by) in
   let k2   = dist /. phi in
   let k1   = dist -. k2 in
   let sum  = k1 +. k2 in
   (*         x                 ,           y             *)
-  ((k1*.p2x +. k2*.p1x)/. sum , (k1*.p2y +. k2*.p1y)/. sum);;
+  ((k1*.bx +. k2*.ax)/. sum , (k1*.by +. k2*.ay)/. sum);;
 
 let integer_triangle (t : triangle) =
   let apply_to_pair f (x, y) = (f x, f y)
@@ -55,47 +56,40 @@ let draw_triangle points =
   let (a,b,c)= integer_triangle points in
   fill_poly [|a;b;c|];;
 
+
+
 (*------------Triangle divider algorithme for penrose paving------------
   pre-cond:
     p: - apex is always the first point
        - points aren't aligned
 *)
 let rec divide generation (t : penrose_triangle) =
-  if generation <= 0 then 
+  match t with
+  |(triangle,_) when generation<=0 -> 
+    set_random_color();
+    draw_triangle triangle ;
+
+  |((apex,s1,s2),Obtuse) ->
+    let btw = split_line s2 s1 in
+    let a = (s1, apex, btw) 
+    and o = (btw, apex, s2) in
     begin
-      set_random_color();
-      draw_triangle t.points;
+      divide (generation-1) (a,Acute);
+      divide (generation-1) (o,Obtuse);
     end
-  else
-    match t with
-    | {ttype=Obtuse ; points=(apex,s1,s2)} ->
-        let btw = split_line s2 s1 in
-        let a = (s1, apex, btw) 
-        and o = (btw, apex, s2) in
-        begin
-          divide (generation-1) {points=a;ttype=Acute};
-          divide (generation-1) {points=o;ttype=Obtuse};
-        end
-    | {ttype=_ ; points=(apex,s1,s2)} ->
-        let btw = split_line s1 apex
-        and o_h = split_line apex s2 in
-        let a1 = (s2, btw, s1) 
-        and a2 = (s2, btw, o_h) 
-        and o  = (o_h, btw, apex) in
-        begin
-          divide (generation-1) {points=a1;ttype=Acute};
-          divide (generation-1) {points=a2;ttype=Acute};
-          divide (generation-1) {points=o;ttype=Obtuse};
-        end;;
 
- 
-(*-------------main--------------------------------*)     
+  |((apex,s1,s2),_) ->
+    let btw = split_line s1 apex
+    and o_h = split_line apex s2 in
+    let a1 = (s2, btw, s1) 
+    and a2 = (s2, btw, o_h) 
+    and o  = (o_h, btw, apex) in
+    begin
+      divide (generation-1) (a1,Acute);
+      divide (generation-1) (a2,Acute);
+      divide (generation-1) (o,Obtuse);
+    end;;
 
-(* Close any possible open window *)
-close_graph();;
-
-(* Create a new window *)
-open_graph (" "^(string_of_int width)^"x"^(string_of_int height)^"+0-0");;
 
 
 (* Initialize random number generator *)
@@ -104,11 +98,16 @@ Random.self_init;;
 (* Setup the first triangle to call it with divide *)
 let s1 = (0.,0.)
 and s2 = (0.,float_of_int height) in
-let dist = (distance s1 s2) in
-let apex = ((sqrt (phi**2. -. 0.25))*.dist , dist/.2.) in
+let dist = (distance s1 s2)
+and height_base_x_ratio = 
+  if start_with_acute_triangle
+  then sqrt (phi**2. -. 0.25)
+  else sqrt (   1.   -. 0.25*.phi**2.) in
+let apex = (height_base_x_ratio*.dist , dist/.2.) in
 let starting_acute_triangle = (apex, s2, s1) in
 
-divide 10 {points=starting_acute_triangle;ttype=Acute};;
+divide 10 (starting_acute_triangle,
+           if start_with_acute_triangle then Acute else Obtuse);;
 
 (*Keep the graph open*)
 ignore (Graphics.read_key ());;
